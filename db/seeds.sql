ed1f59c306d34474b3b038d4c03940594bbac9ec
tho <tho.giangb@gmail.com>
2017-08-09 12:42:04 -0700

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Blue Burger');
+INSERT INTO burgers (burger_name) VALUES ('American Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Uber Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Backyard Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('No Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Yellow Burger');
